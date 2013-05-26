2061b6339278e6e81706d294602d06e3a7051cc9
sakuramilk <c.sakuramilk@gmail.com>
2013-05-26 22:22:17 +0900

    sc02c: fix LOCAL_PATH
    Change-Id: Idd41b31d4e292a974cc2a954b865339c8e26e669

--- sc02c/sc02c-vendor-blobs.mk
+++ sc02c/sc02c-vendor-blobs.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(LOCAL_PATH)
+LOCAL_PATH := vendor/samsung/sc02c
 
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/lib/libakm.so \
