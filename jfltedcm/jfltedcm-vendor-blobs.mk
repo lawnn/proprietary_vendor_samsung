62a485b6a20fc5b4ec23b6c92eddac51126a49b0
HomuHomu <ecoco666@gmail.com>
2013-06-15 22:31:50 +0900

    jfltedcm:Add Felica Acccess(not tested)

--- jfltedcm/jfltedcm-vendor-blobs.mk
+++ jfltedcm/jfltedcm-vendor-blobs.mk
@@ -17,6 +17,31 @@
 PRODUCT_COPY_FILES += \
         vendor/samsung/jfltedcm/proprietary/lib/libemoji_docomo.so:system/lib/libemoji_docomo.so \
         vendor/samsung/jfltedcm/proprietary/fonts/truetypeEmoji.ttf:system/fonts/truetypeEmoji.ttf \
+	vendor/samsung/jfltedcm/proprietary/app/FeliCaLock.apk:system/app/FeliCaLock.apk \
+	vendor/samsung/jfltedcm/proprietary/app/FeliCaRemoteLock.apk:system/app/FeliCaRemoteLock.apk \
+	vendor/samsung/jfltedcm/proprietary/app/FeliCaTest.apk:system/app/FeliCaTest.apk \
+	vendor/samsung/jfltedcm/proprietary/app/MobileFeliCaClient.apk:system/app/MobileFeliCaClient.apk \
+	vendor/samsung/jfltedcm/proprietary/app/MobileFeliCaMenuApp.apk:system/app/MobileFeliCaMenuApp.apk \
+	vendor/samsung/jfltedcm/proprietary/app/MobileFeliCaSettingApp.apk:system/app/MobileFeliCaSettingApp.apk \
+	vendor/samsung/jfltedcm/proprietary/app/MobileFeliCaWebPluginBoot.apk:system/app/MobileFeliCaWebPluginBoot.apk \
+	vendor/samsung/jfltedcm/proprietary/app/FdtpApplication.apk:system/app/FdtpApplication.apk \
+	vendor/samsung/jfltedcm/proprietary/app/FdtpMfcAccess.apk:system/app/FdtpMfcAccess.apk \
+	vendor/samsung/jfltedcm/proprietary/bin/mfdp:system/bin/mfdp \
+	vendor/samsung/jfltedcm/proprietary/bin/mfsc:system/bin/mfsc \
+	vendor/samsung/jfltedcm/proprietary/lib/libcordon.so:system/lib/libcordon.so \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/No_ticket.png:system/etc/felica/No_ticket.png \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/air.png:system/etc/felica/air.png \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/bookmark.cfg:system/etc/felica/bookmark.cfg \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/common.cfg:system/etc/felica/common.cfg \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/id.png:system/etc/felica/id.png \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/mfc_falp.cfg:system/etc/felica/mfc_falp.cfg \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/mfc_push.cfg:system/etc/felica/mfc_push.cfg \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/mfm.cfg:system/etc/felica/mfm.cfg \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/mfs.cfg:system/etc/felica/mfs.cfg \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/money.png:system/etc/felica/money.png \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/portal_icon.png:system/etc/felica/portal_icon.png \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/toku.png:system/etc/felica/toku.png \
+	vendor/samsung/jfltedcm/proprietary/etc/felica/train.png:system/etc/felica/train.png \
         vendor/samsung/jfltedcm/proprietary/app/SPModePushNotifier.apk:system/app/SPModePushNotifier.apk \
         vendor/samsung/jfltedcm/proprietary/etc/apns-conf.xml:system/etc/apns-conf.xml \
         vendor/samsung/jfltedcm/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
