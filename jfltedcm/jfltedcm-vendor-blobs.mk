aa4114f232bc4802a90678a5932fa596fe8f8d06
sakuramilk <c.sakuramilk@gmail.com>
2014-02-22 06:54:31 +0900

    jfltedcm: add sony nfc libs
    Change-Id: Icea32537aa3adb03d2bf9c201f9584ad3cf7eda0

--- jfltedcm/jfltedcm-vendor-blobs.mk
+++ jfltedcm/jfltedcm-vendor-blobs.mk
@@ -44,4 +44,7 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/jfltedcm/proprietary/etc/felica/train.png:system/etc/felica/train.png \
         vendor/samsung/jfltedcm/proprietary/app/SPModePushNotifier.apk:system/app/SPModePushNotifier.apk \
         vendor/samsung/jfltedcm/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
-        vendor/samsung/jfltedcm/proprietary/lib/libril.so:system/lib/libril.so
+        vendor/samsung/jfltedcm/proprietary/lib/libril.so:system/lib/libril.so \
+        vendor/samsung/jfltedcm/proprietary/lib/libnfc_sony.so:system/lib/libnfc_sony.so \
+        vendor/samsung/jfltedcm/proprietary/lib/libnfc_sony_jni.so:system/lib/libnfc_sony_jni.so \
+        vendor/samsung/jfltedcm/proprietary/lib/libnfc_sony_ndef.so:system/lib/libnfc_sony_ndef.so
