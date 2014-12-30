275989ad59d9712bd7903dbae3805f9ab2ab7273
scoty755 <scoty755@gmail.com>
2014-12-30 09:16:33 +0900

    klte: felica-common: Define FeliCa module in android.mk

--- klte-felica-common/felica-common-vendor.mk
+++ klte-felica-common/felica-common-vendor.mk
@@ -12,4 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_PACKAGES += \
+    FeliCaLock \
+    FeliCaRemoteLock \
+    FeliCaTest \
+    MobileFeliCaClient \
+    MobileFeliCaMenuApp \
+    MobileFeliCaSettingApp \
+    MobileFeliCaWebPluginBoot
+
 $(call inherit-product, vendor/samsung/klte-felica-common/felica-common-vendor-blobs.mk)
