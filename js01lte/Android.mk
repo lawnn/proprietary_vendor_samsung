2e45c994252410a04927ca8e9a8b579f8811dd0d
lawn <bloom.all.over.12@gmail.com>
2016-10-24 03:50:07 +0900

    js01lte: Update blobs GPG1

--- js01lte/Android.mk
+++ js01lte/Android.mk
@@ -17,16 +17,5 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),js01lte)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := platform
-include $(BUILD_PREBUILT)
-
 endif
 
